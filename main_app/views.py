9ec6da2580411c75623fe860a923520c8f53b51b
hnhtran <tran_eetas@yahoo.com>
2022-07-24 15:10:44 +1000

    about me page created

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def home(request):
+    return HttpResponse("Hello, friends. You are at the front of my door. Please knock.")
+
+def about(request):
+    # return HttpResponse("This is the about page. I am a software engineer.")
+    return render(request, 'about.html')
+
+def projects(request):
+    return render(request, 'projects.html')
