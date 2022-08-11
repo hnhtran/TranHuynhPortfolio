6028955fc98e44e70392fdecfbbac32974ca6533
hnhtran <tran_eetas@yahoo.com>
2022-08-11 13:05:46 +1000

    add admin control for feedback

--- main_app/models.py
+++ main_app/models.py
@@ -3,3 +3,11 @@ from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
 # Create your models here.
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    message = models.TextField()
+    date = models.DateField(default=date.today)
+
+    def __str__(self):
+        return self.name
