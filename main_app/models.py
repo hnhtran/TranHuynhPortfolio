7a8518efcd47f229ae0a4ba3a6305bca7988c5a2
hnhtran <tran_eetas@yahoo.com>
2022-08-11 13:18:25 +1000

    change feedback model

--- main_app/models.py
+++ main_app/models.py
@@ -5,16 +5,17 @@ from django.contrib.auth.models import User
 # Create your models here.
 # Tuple for feedback type choices
 FEEDBACK_TYPE_CHOICES = (
-    ('G', 'I like it!'),
-    ('NT', 'Everything need to be improved'),
-    ('CSS', 'Need style improvement'),
-    ('FT', 'Need features improvement'),
+    ('GO', 'I like it!'),
+    ('NO', 'Everything need to be improved'),
+    ('SI', 'Need style improvement'),
+    ('FI', 'Need features improvement'),
 )
 class Feedback(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
     message = models.TextField()
+    feedback = models.CharField(max_length=2, choices=FEEDBACK_TYPE_CHOICES, default='GO')
     date = models.DateField(default=date.today)
 
     def __str__(self):
-        return self.name
+        return f'{self.get_feedback_display()} on {self.date}- {self.message}'
