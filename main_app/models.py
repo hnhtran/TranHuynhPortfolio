a24089f8e48af203d91d1f2160c3018697c5d2a7
hnhtran <tran_eetas@yahoo.com>
2022-08-11 13:13:01 +1000

    add tuple for field.choices

--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,13 @@ from django.urls import reverse
 from datetime import date
 from django.contrib.auth.models import User
 # Create your models here.
+# Tuple for feedback type choices
+FEEDBACK_TYPE_CHOICES = (
+    ('G', 'I like it!'),
+    ('NT', 'Everything need to be improved'),
+    ('CSS', 'Need style improvement'),
+    ('FT', 'Need features improvement'),
+)
 class Feedback(models.Model):
     name = models.CharField(max_length=100)
     email = models.EmailField(max_length=100)
