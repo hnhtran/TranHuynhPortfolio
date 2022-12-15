43d1f3e47786e11d3c65b9fc727f242f243962dd
hnhtran <tran_eetas@yahoo.com>
2022-12-15 11:46:40 +1100

    config before render deploy

--- TranHuynhPortfolio/settings.py
+++ TranHuynhPortfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import dj_database_url
 from distutils.command.config import config
 from pathlib import Path
 import os
@@ -52,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -95,10 +97,12 @@ WSGI_APPLICATION = 'TranHuynhPortfolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'feedback',
-    }
+    'default': dj_database_url.config(
+        # 'ENGINE': 'django.db.backends.postgresql',
+        # 'NAME': 'feedback',
+        default='postgres://twsbe_user:bdJ2clZCx00ln8wG73izLIhFUKZ8QZd2@dpg-ce81qb6n6mpksegf0ed0-a.singapore-postgres.render.com/twsbe',
+        conn_max_age=600
+    )
 }
 
 # Password validation
@@ -136,6 +140,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+if not DEBUG:    # Tell Django to copy statics to the `static files` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on the WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
